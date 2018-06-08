9e8c405a54aca53ac17bfdc66a080b0dbbce42a8
Felipe Garcia <felipegarciaitu@hotmail.com>
2018-06-07 21:50:23 -0300

    Created double AES

---
+++ README.md
@@ -0,0 +1,16 @@
+#DESCRIPTION
+##this program implements a double version of the AES-128
+##main.py: the main file, handle the I/O and calls the modes of the program
+##aes.py: the AES-128 implementation
+
+#INSTRUCTIONS
+##to run in cipher mode, type: python main.py C keyfile.txt plaintext.txt encriptedtext.txt
+##to run in decript mode, type: python main.py D keyfile.txt encriptedtext.txt decriptedtext.txt
+##to run in generate key mode: python main.py K keyfile.txt 1234567
+###for cipher mode and decript mode, keyfile.txt must exist
+###in C mode, plaintext.txt must exist
+###in D mode, encriptedtext.txt must exist
+
+#REQUIREMENTS
+##python3
+##math
--- main.py
+++ main.py
@@ -15,12 +15,10 @@ if __name__ == '__main__':
 			#reading key file
 			with open(keyf_name, 'r') as key_file:
 				key = key_file.read()
+				while len(key) < 32:
+					key += key[0]
 				key1 = key[0:16]
 				key2 = key[16:32]
-				key3 = key[32:48]
-				print(key1)
-				print(key2)
-				print(key3)
 			#reading plain text file
 			with open(plain_text_file_name, 'r') as plain_text_file:
 				plain_text = plain_text_file.read()
@@ -29,7 +27,6 @@ if __name__ == '__main__':
 			with open(encrpited_text_file_name, 'w') as encrpited_text_file:
 				encripted_text = aes.encript(plain_text, key1)
 				encripted_text = aes.encript(encripted_text, key2)
-				encripted_text = aes.encript(encripted_text, key3)
 				encrpited_text_file.write(encripted_text)
 			print(plain_text)
 			print(encripted_text)
@@ -46,14 +43,14 @@ if __name__ == '__main__':
 			encrpited_text_file_name = sys.argv[3]
 			with open(keyf_name, 'r') as key_file:
 				key = key_file.read()
+				while len(key) < 32:
+					key += key[0]
 				key1 = key[0:16]
 				key2 = key[16:32]
-				key3 = key[32:48]
 			with open(encrpited_text_file_name, 'r') as encrpited_text_file:
 				encripted_text = encrpited_text_file.read()
 			with open(plain_text_file_name, 'w') as plain_text_file:
-				plain_text = aes.decript(encripted_text, key3)
-				plain_text = aes.decript(plain_text, key2)
+				plain_text = aes.decript(encripted_text, key2)
 				plain_text = aes.decript(plain_text, key1)
 				plain_text_file.write(plain_text)
 			print(encripted_text)
